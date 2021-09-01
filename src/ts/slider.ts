c209dafb5b09bdac2d536d59dd1b870e74fbf6ef
Roman Belov <rnbelov96@gmail.com>
2021-09-01 17:39:36 +0300

    Редактирует слайдер

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -1,106 +1,184 @@
 export {};
 
-const imagesLength = 4;
-let currentImage = 1;
-const mode = 1;
-
-const translateXPosList = [
-  {
-    mode: 3,
-    pos: -100,
-  },
-  {
-    mode: 2,
-    pos: -200,
-  },
-  {
-    mode: 1,
-    pos: -400,
-  },
-];
-const initTranslateXPos = translateXPosList.find(el => el.mode === mode)
-  ?.pos as number;
-let translateXPos = initTranslateXPos;
-const translateStep = 100 / mode;
-let offset: number;
-let posInit: number;
-let isDragging = false;
-
-const imagesBoxEl = document.querySelector(
-  '.slider__img-box',
-) as HTMLDivElement;
-const wrapperEl = document.querySelector('.slider__wrapper') as HTMLDivElement;
-
-let wrapperCoords = wrapperEl.getBoundingClientRect();
-let wrapperLeftCoords = wrapperCoords.left;
-let wrapperWidth = wrapperCoords.width;
-
-const prevBtnEl = document.querySelector(
-  '.slider__btn-prev',
-) as HTMLButtonElement;
-const nextBtnEl = document.querySelector(
-  '.slider__btn-next',
-) as HTMLButtonElement;
-
-const navBoxEl = document.querySelector('.slider__nav-box') as HTMLDivElement;
-
-let navItemList: HTMLButtonElement[];
-
-const blockBtns = () => {
-  nextBtnEl.disabled = true;
-  prevBtnEl.disabled = true;
-  navItemList.forEach(el => {
-    // eslint-disable-next-line no-param-reassign
-    el.disabled = true;
-  });
+let option = {
+  imagesLength: 4,
+  maxMode: 2,
+  isFlexible: true,
+  sliderSectionName: '',
+  changeModeBreakpoints: [575, 991],
+  withButtons: true,
+  withNav: true,
 };
 
-const activateBtns = () => {
-  nextBtnEl.disabled = false;
-  prevBtnEl.disabled = false;
-  navItemList.forEach(el => {
-    // eslint-disable-next-line no-param-reassign
-    el.disabled = false;
-  });
-};
-
-navItemList = Array(imagesLength)
-  .fill({}, 0, imagesLength)
-  .map((_el, index) => {
-    const navItemEl = document.createElement('button');
-    navItemEl.classList.add('slider__nav-item');
-    if (index === 0) {
-      navItemEl.classList.add('slider__nav-item_active');
+const initSlider = (options: {
+  imagesLength: number;
+  maxMode: number;
+  isFlexible: boolean;
+  sliderSectionName: string;
+  changeModeBreakpoints: Array<number>;
+  withButtons: boolean;
+  withNav: boolean;
+}) => {
+  const getMode = (pageWidth: number) => {
+    if (!options.isFlexible) {
+      return options.maxMode;
     }
-    navItemEl.dataset.image = String(index + 1);
-    navItemEl.addEventListener('click', e => {
-      blockBtns();
-      const navEl = e.currentTarget as HTMLDivElement;
-      imagesBoxEl.style.transition = 'transform .5s';
-      const prevCurrentImage = currentImage;
-      currentImage = Number(navEl.dataset.image);
-      const newTranslateXPos = initTranslateXPos - translateStep * (currentImage - 1);
+
+    if (options.maxMode === 3) {
+      if (pageWidth <= options.changeModeBreakpoints[0]) {
+        return 1;
+      }
+      if (
+        pageWidth > options.changeModeBreakpoints[0] &&
+        pageWidth <= options.changeModeBreakpoints[1]
+      ) {
+        return 2;
+      }
+      return 3;
+    }
+
+    if (options.maxMode === 2) {
+      if (pageWidth <= options.changeModeBreakpoints[0]) {
+        return 1;
+      }
+      return 2;
+    }
+
+    return 1;
+  };
+
+  let currentImage = 1;
+  let pageWidth = document.documentElement.scrollWidth;
+  let mode = getMode(pageWidth);
+
+  const translateXPosList = [
+    {
+      mode: 3,
+      pos: -100,
+    },
+    {
+      mode: 2,
+      pos: -200,
+    },
+    {
+      mode: 1,
+      pos: -400,
+    },
+  ];
+  let initTranslateXPos = translateXPosList.find(el => el.mode === mode)
+    ?.pos as number;
+  let translateXPos = initTranslateXPos;
+  let translateStep = 100 / mode;
+  let offset: number;
+  let posInit: number;
+  let isDragging = false;
+
+  const imagesBoxEl = document.querySelector(
+    `.${options.sliderSectionName}__img-box`,
+  ) as HTMLDivElement;
+  const wrapperEl = document.querySelector(
+    `.${options.sliderSectionName}__wrapper`,
+  ) as HTMLDivElement;
+
+  let wrapperCoords = wrapperEl.getBoundingClientRect();
+  let wrapperLeftCoords = wrapperCoords.left;
+  let wrapperWidth = wrapperCoords.width;
+
+  let prevBtnEl: HTMLButtonElement;
+  let nextBtnEl: HTMLButtonElement;
+  if (options.withButtons) {
+    prevBtnEl = document.querySelector(
+      `.${options.sliderSectionName}__btn-prev`,
+    ) as HTMLButtonElement;
+    nextBtnEl = document.querySelector(
+      `.${options.sliderSectionName}__btn-next`,
+    ) as HTMLButtonElement;
+  }
+
+  let navBoxEl: HTMLDivElement;
+  let navItemList: HTMLButtonElement[];
+  if (options.withNav) {
+    navBoxEl = document.querySelector(
+      `.${options.sliderSectionName}__nav-box`,
+    ) as HTMLDivElement;
+    navItemList = Array(options.imagesLength)
+      .fill({}, 0, options.imagesLength)
+      .map((_el, index) => {
+        const navItemEl = document.createElement('button');
+        navItemEl.classList.add(`${options.sliderSectionName}__nav-item`);
+        if (index === 0) {
+          navItemEl.classList.add(
+            `${options.sliderSectionName}__nav-item_active`,
+          );
+        }
+        navItemEl.dataset.image = String(index + 1);
+        navItemEl.addEventListener('click', e => {
+          blockBtns();
+          const navEl = e.currentTarget as HTMLDivElement;
+          imagesBoxEl.style.transition = 'transform .5s';
+          const prevCurrentImage = currentImage;
+          currentImage = Number(navEl.dataset.image);
+          const newTranslateXPos =
+            initTranslateXPos - translateStep * (currentImage - 1);
+          translateXPos = newTranslateXPos;
+          imagesBoxEl.style.transform = `translate3d(${translateXPos}%, 0px, 0px)`;
+          navItemList[currentImage - 1].classList.add(
+            `${options.sliderSectionName}__nav-item_active`,
+          );
+          navItemList[prevCurrentImage - 1].classList.remove(
+            `${options.sliderSectionName}__nav-item_active`,
+          );
+          setTimeout(() => {
+            imagesBoxEl.style.transition = '';
+            activateBtns();
+          }, 500);
+        });
+        return navItemEl;
+      });
+
+    navBoxEl.append(...navItemList);
+  }
+
+  const blockBtns = () => {
+    nextBtnEl.disabled = true;
+    prevBtnEl.disabled = true;
+    navItemList.forEach(el => {
+      // eslint-disable-next-line no-param-reassign
+      el.disabled = true;
+    });
+  };
+
+  const activateBtns = () => {
+    nextBtnEl.disabled = false;
+    prevBtnEl.disabled = false;
+    navItemList.forEach(el => {
+      // eslint-disable-next-line no-param-reassign
+      el.disabled = false;
+    });
+  };
+
+  window.addEventListener('resize', () => {
+    wrapperCoords = wrapperEl.getBoundingClientRect();
+    wrapperLeftCoords = wrapperCoords.left;
+    wrapperWidth = wrapperCoords.width;
+
+    if (options.isFlexible) {
+      pageWidth = document.documentElement.scrollWidth;
+      const newMode = getMode(pageWidth);
+      if (mode === newMode) {
+        return;
+      }
+      mode = newMode;
+      translateStep = 100 / mode;
+      initTranslateXPos = translateXPosList.find(el => el.mode === mode)
+        ?.pos as number;
+      const newTranslateXPos =
+        initTranslateXPos - translateStep * (currentImage - 1);
       translateXPos = newTranslateXPos;
       imagesBoxEl.style.transform = `translate3d(${translateXPos}%, 0px, 0px)`;
-      navItemList[currentImage - 1].classList.add('slider__nav-item_active');
-      navItemList[prevCurrentImage - 1].classList.remove(
-        'slider__nav-item_active',
-      );
-      setTimeout(() => {
-        imagesBoxEl.style.transition = '';
-        activateBtns();
-      }, 500);
-    });
-    return navItemEl;
+    }
   });
-
-navBoxEl.append(...navItemList);
-
-window.addEventListener('resize', () => {
-  wrapperCoords = wrapperEl.getBoundingClientRect();
-  wrapperLeftCoords = wrapperCoords.left;
-  wrapperWidth = wrapperCoords.width;
-});
+};
 
 const dragAction = (e: MouseEvent) => {
   const posX = e.pageX - wrapperLeftCoords;
